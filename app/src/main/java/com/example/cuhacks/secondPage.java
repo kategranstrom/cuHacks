87b2ce1fd1eac737405f1b91f7e7149e520cd154
Kate Granstrom <kdgranstrom@uwaterloo.ca>
2019-02-16 18:08:23 -0500

    "start button"

---
+++ app/src/main/java/com/example/cuhacks/secondPage.java
@@ -0,0 +1,13 @@
+package com.example.cuhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+}
