68eb6131da0df25bde0e2645fe9854bd2adac3d8
Nirda <nirda_ali@hotmail.com>
2019-02-17 03:36:16 -0500

    Merge branch 'master' of C:\Users\Nirda\cuHacks with conflicts.

--- app/src/main/java/com/example/cuhacks/Summary.java
+++ app/src/main/java/com/example/cuhacks/Summary.java
@@ -6,36 +6,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 
 public class Summary extends AppCompatActivity {
+
+    int income_1;
+    int income_2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
-        //Intent intent = getIntent();
-        int message = getIntent().getIntExtra("EXTRA_INT1", 0);
-        String fmessage = Integer.toString(message);
-        TextView textView = findViewById(R.id.textView9);
-        textView.setText(fmessage);
-        int message1 = getIntent().getIntExtra("EXTRA_INT2", 0);
-        String fmessage1 = Integer.toString(message1);
-        TextView textView1 = findViewById(R.id.textView10);
-        textView1.setText(fmessage1);
-        //int message2 = getIntent().getIntExtra("EXTRA_INT3", 0);
-        //String fmessage2 = Integer.toString(message2);
-        //TextView textView2 = findViewById(R.id.textView11);
-        //textView2.setText(fmessage2);
-        int message2 = getIntent().getIntExtra("EXTRA_INT3", 8);
-        String fmessage2 = Integer.toString(message2);
-        TextView textView2 = findViewById(R.id.textView11);
-        textView2.setText(fmessage2);
-        int message3 = getIntent().getIntExtra("EXTRA_INT4", 8);
-        String fmessage3 = Integer.toString(message3);
-        TextView textView3 = findViewById(R.id.textView12);
-        textView3.setText(fmessage3);
     }
 
     public void editIncome(View view) {
@@ -51,12 +33,16 @@ public class Summary extends AppCompatActivity {
     public void goBack(View view) {
         Intent intent = new Intent(this, thirdPage.class);
         startActivity(intent);
+
     }
 
-    public void calculate(View view) {
-        Intent intent = new Intent(this, totalpage.class);
-        startActivity(intent);
-    }
+    public void calculateNow(View view){
 
+        income_1 = getIntent().getIntExtra("EXTRA_INT1",0);
+        income_2 = getIntent().getIntExtra("EXTRA_INT2",0);
+
+
+
+    }
 }
 
