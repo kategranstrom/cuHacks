09cae8db14b369b2bfa281388d1b541926b84f3f
Dez1999 <blake_db99@outlook.com>
2019-02-17 00:31:16 -0500

    Summary Page: Added buttons

--- app/src/main/java/com/example/cuhacks/Summary.java
+++ app/src/main/java/com/example/cuhacks/Summary.java
@@ -1,7 +1,12 @@
 package com.example.cuhacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Summary extends AppCompatActivity {
 
@@ -10,6 +15,13 @@ public class Summary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
     }
-}
 
+    public void editIncome (View view){
+            Intent intent = new Intent(this, secondPage.class);
+            startActivity(intent);
+        }
+
+
+
+        }
 
